30d56eba44783c834f7c0f7ba4230df690e601a3
Emil Stjernlöf <emil.stjernlof@gmail.com>
2023-05-02 15:31:36 +0200

    Implemented test. Not done

---
+++ src/components/button/logic/Logic.js
@@ -0,0 +1,19 @@
+
+
+const compareChoices = (user, computer) => {
+
+    let result = "";
+
+    if(computer == user) {
+      result = "Oavgjort!"
+    } else if ((computer === 2 && user === 3) || (computer === 3 && user === 1) || (computer === 3 && user === 2)) {
+      result="Du har vunnit!"
+    } else {
+      result = "Du har förlorat!"
+    }
+    return result;
+
+  }  
+
+export default compareChoices
+
--- src/components/button/result/Result.js
+++ src/components/button/result/Result.js
@@ -1,15 +1,33 @@
 import React from 'react'
+import Logic from '../logic/Logic'
 
 const Result = ({ computerChoise, userChoise}) => {
 
+  // let result = "";
+
+  // const compareChoices = (user, computer) => {
+  //   if(computer == user) {
+  //     result = "Oavgjort!"
+  //   } else if ((computer === 2 && user === 3) || (computer === 3 && user === 1) || (computer === 3 && user === 2)) {
+  //     result="Du har vunnit!"
+  //   } else {
+  //     result = "Du har förlorat!"
+  //   }
+  //   return result;
+
+  // }  
+  
   return (
     <div>
     <h2>Datorn valde: {computerChoise} Du valde: {userChoise}</h2>
-      {computerChoise === userChoise 
-      ? <h3>Oavgjort</h3>
-      : (computerChoise === 2 && userChoise === 3) || (computerChoise === 3 && userChoise === 1) || (computerChoise === 3 && userChoise === 2)
+    <h3><Logic computerChoise={computerChoise} userChoise={userChoise}/></h3>
+      {/* {computerChoise === userChoise 
+      ? <h3>{result}</h3>
+      : (computerChoise === 2 && userChoise === 3)
+      || (computerChoise === 3 && userChoise === 1) 
+      || (computerChoise === 3 && userChoise === 2)
       ? <h3>Du har vunnit!</h3>
-      : <h3>Du har förlorat</h3>}
+      : <h3>Du har förlorat</h3>} */}
     </div>
   )
 }
