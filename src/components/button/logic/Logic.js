2ed398733da946806d9bcba628af045730374582
Tobias Dedorsson <50805430+tobionesies@users.noreply.github.com>
2023-05-03 12:39:48 +0200

    updates to test suite

--- src/components/button/logic/Logic.js
+++ src/components/button/logic/Logic.js
@@ -1,12 +1,40 @@
 
 
-const compareChoices = ( {user, computer} ) => {
+// const compareChoices = ({user, computer}) => {
 
+//     console.log(user)
+//     console.log(computer)
+//     let result = "";
+
+//     if(computer === user) {
+//       result = "Oavgjort!"
+//     } else if ((computer === 2 && user === 3) || (computer === 3 && user === 1) || (computer === 3 && user === 2)) {
+//       result="Du har vunnit!"
+//     } else {
+//       result = "Du har förlorat!"
+//     }
+
+//     return result;
+
+//   }  
+
+// export default compareChoices
+
+import React from 'react'
+
+const Logic = ({user, computer}) => {
+  const user2 = user
+  const computer2 = computer
+
+  const compareChoices = (user2, computer2) => {
+
+    console.log(user2)
+    console.log(computer2)
     let result = "";
 
-    if(computer === user) {
+    if(computer2 === user2) {
       result = "Oavgjort!"
-    } else if ((computer === 2 && user === 3) || (computer === 3 && user === 1) || (computer === 3 && user === 2)) {
+    } else if ((computer2 === 2 && user2 === 3) || (computer2 === 3 && user2 === 1) || (computer2 === 3 && user2 === 2)) {
       result="Du har vunnit!"
     } else {
       result = "Du har förlorat!"
@@ -15,6 +43,11 @@ const compareChoices = ( {user, computer} ) => {
     return result;
 
   }  
+  return (
+    <>
+      {compareChoices(user, computer)}
+    </>
+  )
+}
 
-export default compareChoices
-
+export default Logic
---
+++ src/components/button/logic/_test_/Logic.test.js
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/react"
+import Logic from "../Logic"
+
+describe("Result", () => {
+        // 1=sax
+        // 2=sten
+        // 3=papper
+    test("Should win if papper vs rock", () => {
+        //Given
+        const user = 3
+        const computer = 2
+    
+        //When
+       render(<Logic user={user} computer={computer} />)
+
+       const result = screen.getByText(/vunnit!/i)
+
+        //Then
+        expect(result).toBeInTheDocument()
+    })
+    test("Should loose when scissor vs rock", () => {
+        //Given
+        const user = 1
+        const computer = 1
+    
+        //When
+       render(<Logic user={user} computer={computer} />)
+
+       const result = screen.queryByRole("heading", {level: 3})
+
+        //Then
+        expect(result).toHaveTextContent('Du har förlorat!');
+        // expect(result).toBeInTheDocument()
+    })
+    test("Should be get a draw", () => {
+        //Given
+        const user = 1
+        const computer = 1
+    
+        //When
+       render(<Logic user={user} computer={computer} />)
+
+       const result = screen.getByText(/Oavgjort/i)
+
+        //Then
+        expect(result).toBeInTheDocument()
+    })
+})
